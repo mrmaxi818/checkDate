0a72b72bff0c83f2d090df397b8bf5c61ccaf7a5
Schuyler Maxey <laptop@Schuylers-MBP.attlocal.net>
2019-04-10 20:59:59 -0700

    get react app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function DateApp(){
+const DateApp = () => {
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay
